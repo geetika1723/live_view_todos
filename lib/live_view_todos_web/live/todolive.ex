95d6e9ce63b5a07d15c486f20cda8375391a7ea1
geetika1723 <geetikasethi1723@gmail.com>
2022-07-27 16:42:50 +0530

    Developing to do list app

---
+++ lib/live_view_todos_web/live/todolive.ex
@@ -0,0 +1,10 @@
+defmodule LiveViewTodosWeb.TodoLive do
+  use LiveViewTodosWeb, :live_view
+  alias LiveViewTodos.Todos
+
+  def mount(_params, _session, socket) do
+    {:ok, assign(socket, todos: Todos.list_todos())}
+  end
+
+
+end
