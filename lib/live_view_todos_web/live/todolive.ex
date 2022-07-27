def208371e83a7b6a74705858c9ae1da29980559
geetika1723 <geetikasethi1723@gmail.com>
2022-07-27 18:57:33 +0530

    broadcast done

--- lib/live_view_todos_web/live/todolive.ex
+++ lib/live_view_todos_web/live/todolive.ex
@@ -3,8 +3,21 @@ defmodule LiveViewTodosWeb.TodoLive do
   alias LiveViewTodos.Todos
 
   def mount(_params, _session, socket) do
+    Todos.subscribe()
+
     {:ok, assign(socket, todos: Todos.list_todos())}
   end
+  def handle_event("add", %{"todo"=> todo}, socket) do
+    Todos.create_todo(todo)
+    {:noreply, assign(socket, todos: Todos.list_todos()) }
+  end
 
-
+  def handle_info({Todos, [:todo | _], _}, socket) do
+    {:noreply, assign(socket, todos: Todos.list_todos())}
+  end
+  def handle_event("toggle_done", %{"id" => id}, socket) do
+    todo = Todos.get_todo!(id)
+    Todos.update_todo(todo, %{done: !todo.done})
+    {:noreply, assign(socket, todos: Todos.list_todos())}
+  end
 end
